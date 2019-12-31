0e55e75e5a291386e5507920a3e56451b9cf7137
Daniel Urbanowicz <urbanowicz.daniel@gmail.com>
2019-12-31 08:46:41 +0100

    Add postgres and changes in profile and register

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'jarod',
+    password : 'pretender888',
+    database : 'smart-brain'
+  }
+});
+
+
 
 const app = express();
-
 
 const database = {
   users: [
@@ -45,29 +57,29 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    // password: password,
-    entries: 0,
-    joined: new Date()
-  });
-  res.json(database.users[database.users.length - 1]);
+  db('users')
+    .returning('*')  
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date()
+  }).then(user => {
+    res.json(user[0]);
+  })
+    .catch(err => res.status(400).json('unable to register'))
 });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(404).json('no such user');
-  }
+  db.select('*').from('users').where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        throw new Error('Not found user')
+      }
+    })
+    .catch(err => res.status(404).json(err.message))
 });
 
 app.put('/image', (req, res) => {
